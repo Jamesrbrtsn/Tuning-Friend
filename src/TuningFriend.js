bf377308617b763bc780c8ee03669f0ea200384a
James Robertson <James.M.Robertson.Work@gmail.com>
2020-06-05 19:03:39 -0400

    Initial
    Setup initial app component, within the create-react-app component

---
+++ src/TuningFriend.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './TuningFriend.css'
+
+export default function TuningFriend(){
+
+    return (
+        <div id="tuning-friend">
+            <div>
+                <h1>Tuning Friend</h1>
+                <p>Welcome! Let's tune</p>
+            </div>
+        </div>
+    )
+}
